c6fb9fa58c8376dbc9ea2fec3a5b59bc0b0580e0
Insidium <ryanchallen@outlook.com>
2020-04-16 19:53:53 +1000

    Began refining media queries and added smooth scroll JS animations

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
-  $(".menu-toggler").on("click", function () {
-    $(this).toggleClass("open");
-    $(".top-nav").toggleClass("open");
+  $('.menu-toggler').on('click', function () {
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
   });
-});
+
+  $('.top-nav .nav-link').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  });
+
+  $('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100
+    }, 2000);
+  });
+}); 
