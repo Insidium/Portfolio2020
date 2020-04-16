5132977431190837f1110db203ca67750bb6693e
Insidium <ryanchallen@outlook.com>
2020-04-16 20:07:26 +1000

    Added smooth scroll animation for up button

--- main.js
+++ main.js
@@ -14,4 +14,16 @@ $(document).ready(function () {
       scrollTop: $($(this).attr('href')).offset().top - 100
     }, 2000);
   });
+
+  $('#up').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 2000);
+  });
+
+  AOS.init({
+    easing: 'ease',
+    duration:1800
+  });
+
 }); 
